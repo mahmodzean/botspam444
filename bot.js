9ffa1b6bd25993697b8309eacec3e050b9b609ff
mahmodzean <44239884+mahmodzean@users.noreply.github.com>
2018-11-17 01:55:27 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -15,7 +15,7 @@ client.on("message", function(message) {
     var args = message.content.split(/ +/g);
     var command = args.shift()
     
-    if(command == ".8s") {
+    if(command == ".9s") {
         message.channel.send(args.slice(1, args.length).join(" "))    
     }
 });
